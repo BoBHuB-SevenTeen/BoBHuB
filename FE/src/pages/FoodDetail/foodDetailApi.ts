ea8c263e210e9876c0fd7fc024ddb0ea33288641
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-02-12 16:46:15 +0900

    Refactor : 댓글 Update로직 삭제

--- FE/src/pages/FoodDetail/foodDetailApi.ts
+++ FE/src/pages/FoodDetail/foodDetailApi.ts
@@ -7,13 +7,7 @@ export const getShop = async (shopId: number) => await API.get(`/api/shops/${sho
 export const getMenu = async (shopId: number) => await API.get(`/api/food?shopId=${shopId}`);
 
 export const postComment = async (comment: PostComment) => {
-  try {
-    const res = await API.post('/api/comments', comment);
-    if (!res) throw new Error('이미 댓글을 작성하셨습니다.');
-    return res;
-  } catch (err) {
-    alert(err);
-  }
+  return await API.post('/api/comments', comment);
 };
 
 export const patchComment = async (comment: PostComment, commentId: number) => {
