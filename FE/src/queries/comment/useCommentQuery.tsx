124b65e2505bb30cd5b185cca1a161297312c9fb
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-02-14 16:50:54 +0900

    Refactor : comment, menu, shop 쿼리 커스텀훅으로 분리

--- FE/src/queries/comment/useCommentQuery.tsx
+++ FE/src/queries/comment/useCommentQuery.tsx
@@ -3,10 +3,17 @@ import { useQuery } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { Tcomment } from '../../type/commentType';
 
-export const fetchComments = async (shopId: number) => {
+const fetchComments = async (shopId: number) => {
   return await API.get(`/api/comments?shopId=${shopId}`);
 };
 
 export const useCommentQuery = (shopId: number) => {
-  return useQuery<Tcomment[], AxiosError>(['comment', shopId], () => fetchComments(shopId));
-}; // 에러타입 명시해줘야함.
+  const {
+    isLoading: isCommentLoading,
+    isError: isCommentError,
+    data: commentState,
+    isSuccess,
+  } = useQuery<Tcomment[], AxiosError>(['comment', shopId], () => fetchComments(shopId));
+
+  return { isCommentLoading, isCommentError, commentState, isSuccess };
+};
---
+++ FE/src/queries/menu/useMenuQuery.ts
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import * as API from '../../api/API';
+import { Menu } from '../../type/menuType';
+
+const fetchMenu = async (shopId: number) => await API.get(`/api/food?shopId=${shopId}`);
+
+export const useMenuQuery = (shopId: number) => {
+  const {
+    isLoading: isMenuLoading,
+    isError: isMenuError,
+    data: menuState = [],
+    isSuccess,
+  } = useQuery<Menu[], AxiosError>(['menu', shopId], () => fetchMenu(shopId));
+
+  return { isMenuLoading, isMenuError, menuState, isSuccess };
+};
