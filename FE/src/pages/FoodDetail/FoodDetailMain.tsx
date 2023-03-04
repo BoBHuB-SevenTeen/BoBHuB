f5503352365eb64d999f922f6bae44008d21c581
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-03-04 22:54:29 +0900

    Fix : Spinner 오타 수정

--- FE/src/pages/FoodDetail/FoodDetailMain.tsx
+++ FE/src/pages/FoodDetail/FoodDetailMain.tsx
@@ -3,14 +3,10 @@ import ErrorBoundary from '../../components/ErrorBoundary';
 import Spinner from '../../components/Spinner';
 import FoodDetail from './FoodDetail';
 
-const FoodDetailCom = lazy(() => import('./FoodDetail'));
-
 const FoodDetailMain = () => {
   return (
     <ErrorBoundary>
-      <Suspense fallback={Spinner}>
-        <FoodDetailCom />
-      </Suspense>
+      <FoodDetail />
     </ErrorBoundary>
   );
 };
